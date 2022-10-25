c4d6c65816aca0976996d57ce9fb2bd377dafbdc
Emin Redzovic <redzovicemin200@gmail.com>
2022-10-25 22:13:28 +0200

    Domaci 2(fizzbuzz,fizz,buzz)

---
+++ Domaci2/script.js
@@ -0,0 +1,19 @@
+var a,b,i,c=0,d=0,e=0;
+document.getElementById('Calculate').addEventListener('click',function(){
+    a = document.getElementById('first').value
+    b = document.getElementById('second').value
+    for(i=a; i<=b ; i++){
+        if(i%5==0 && i%3==0){
+            c=c+1
+        }
+        else if(i%5==0){
+            d=d+1
+        }
+        else if(i%3==0){
+            e=e+1
+        }
+    }
+    document.getElementById('FiveThree').textContent = c;
+    document.getElementById('Five').textContent = d;
+    document.getElementById('Three').textContent = e;
+})
